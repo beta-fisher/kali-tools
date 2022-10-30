162d9178d8bf74734ae4effa9217d8956c5e5a94
beta <ravacbt@gmail.com>
2022-10-30 13:58:22 -0600

    tool

---
+++ kali-tools.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo ""
+echo ""
+echo -e "e\[33m  _  _      _     _____         _     \e[0m"
+echo -e "e\[33m | |/ /__ _| (_) |_   _|__  ___| |___ \e[0m"
+echo -e "e\[33m | ' </ _` | | |   | |/ _ \/ _ \ (_-< \e[0m"
+echo -e "e\[33m |_|\_\__,_|_|_|   |_|\___/\___/_/__/ \e[0m"
+echo ""
+echo ""
+echo -e "\e[33mCreador: beta-fish\e[0m"
+echo -e "\e[33mPrograma para instalar herramientas de kali\e[0m"
+echo -e "\e[33mgithub: \e[0m"
+echo ""
+echo ""
+#Comando para que dure 3 segundos el banner
+sleep 3
+sudo apt-get update
+sudo apt-get install nmap
+sudo apt-get install aircrack-ng
+sudo apt-get install hydra
+sudo apt-get install responder
+sudo apt-get install hash-identifier
+sudo apt-get install enum4linux
+sudo apt-get install dnsmap
+sudo apt-get install skipfish
+sudo apt-get install netdiscover
+sudo apt-get install cisco-torch
+sudo apt-get install impacket-scripts
+sudo apt-get install airgeddon
+sudo apt-get install chkrootkit
+sudo apt-get install john
+sudo apt-get install metasploit-framework
+sudo apt-get install crunch
+sudo apt-get install traceroute
+sudo apt-get install powershell
+sudo apt-get install wireshark
+sudo apt-get install burpsuite
+sudo apt-get install maltego
+
+echo -e "e\[33m FINALIZADO.. e\[0m"
+
+
+
+
+
